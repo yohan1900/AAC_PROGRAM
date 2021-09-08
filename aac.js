239e6fc8634fd3a030a9d83096a59c53bac41858
yohan1900 <47468230+yohan1900@users.noreply.github.com>
2021-09-08 10:51:24 +0900

    start!!
    2021.09.08

--- aac.js
+++ aac.js
@@ -1,4 +1,49 @@
-function play() {
-  let audio = new Audio("sound/춘천 그리고 가을(권민찬 외 10명).mp3");
+function play1() {
+  let audio = new Audio("sound/화장실.mp3");
+  audio.play();
+}
+
+function play2() {
+  let audio = new Audio("sound/껌.mp3");
+  audio.play();
+}
+function play3() {
+  let audio = new Audio("sound/과자.mp3");
+  audio.play();
+}
+function play4() {
+  let audio = new Audio("sound/물.mp3");
+  audio.play();
+}
+function play5() {
+  let audio = new Audio("sound/까투리.mp3");
+  audio.play();
+}
+function play6() {
+  let audio = new Audio("sound/테블릿.mp3");
+  audio.play();
+}
+function play7() {
+  let audio = new Audio("sound/라바.mp3");
+  audio.play();
+}
+function play8() {
+  let audio = new Audio("sound/뽀로로.mp3");
+  audio.play();
+}
+function play9() {
+  let audio = new Audio("sound/가고싶어요.mp3");
+  audio.play();
+}
+function play10() {
+  let audio = new Audio("sound/보고싶어요.mp3");
+  audio.play();
+}
+function play11() {
+  let audio = new Audio("sound/주세요.mp3");
+  audio.play();
+}
+function play12() {
+  let audio = new Audio("sound/잘먹겠습니다.mp3");
   audio.play();
 }
